1040ba23f57243acc9dcb830217a98f6e5c20357
kahiga2244 <kahigakamiru@gmail.com>
2023-01-21 11:27:07 +0300

    correcting the user route

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,17 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
-
 class UserController extends Controller
 {
     //
     function login(Request $req)
     {
         $user = User::where(['email'=>$req->email])->first();
-        return $user->password;
+        // return $user->password;
         if(!$user || !Hash::check($req->password, $user->password))
         {
             return "Username or password is not matched";
